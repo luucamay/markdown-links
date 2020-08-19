53ee8a46e70ec118b0f435ae72ffebf53b2894c2
Imilla Dev <lupemaydanda@gmail.com>
2020-08-19 19:41:56 -0400

    feat: Process file and print results in console

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const pathSytem = require('path');
 const fs = require('fs');
-
+const marked = require('marked');
 
 const isAbsolutePath = (pathToCheck) => {
   const resolvedPath = pathSytem.resolve(pathToCheck);
@@ -14,40 +14,59 @@ const convertToAbosulute = (pathToConvert) => {
 }
 
 const isFolder = (pathToCheck) => fs.lstatSync(pathToCheck).isDirectory();
-const readMDFile = (pathToRead, mycallback) => {
+
+const processMarkdownFile = (pathToRead, mycallback) => {
+  console.log('Processing markdown file...');
   fs.readFile(pathToRead, (err, data) => {
-    if(err){
+    if (err) {
       console.error(err.message);
       console.error(`Sorry I can't read file: ${pathToRead}`);
     }
-    mycallback(data.toString());
+    const linksArray= getLinks(data.toString());
+    //console.log(linksArray);
+    return mycallback(linksArray);
   })
 }
-const getLinks = (pathToProcess) => {
-  console.log('Getting links from file...');
-  readMDFile(pathToProcess, console.log);
+
+const getLinks = (markdownText) => {
+  var links = [];
+  var renderer = new marked.Renderer();
+  renderer.link = function (href, title, text) {
+    links.push({href, text});
+  };
+  // here is where the marked functions creates an html file
+  // from a markdown text and when it is rendering the links
+  // it pushes them to my links array and place undefined in that place
+  marked(markdownText, { renderer });
+  return links;
+}
+
+const printResults = (linksArray) => {
+  linksArray.forEach(linkObj => {
+    const textTruncated =  linkObj.text.substring(0, 50);;
+    console.log('filename', linkObj.href, textTruncated);
+  });
 }
 
 const mdLinks = (path) => {
   console.log('iniciando funcion mdLinks');
   console.log(`Getting absolute path of ${path}...`);
 
-  if(!isAbsolutePath(path)){
+  if (!isAbsolutePath(path)) {
     path = convertToAbosulute(path);
   }
 
   console.info(`is ${path} file or folder?`);
   try {
-    if(isFolder(path)){
+    if (isFolder(path)) {
       console.log('path is a folder');
     } else {
-      if(path.slice(-2) !== 'md'){
+      if (path.slice(-2) !== 'md') {
         console.log("Sorry, I can't process a file with a extension different to .md")
         return 'error code?';
       } else {
-        console.log('Processing markdown file...');
         // the next function resolves in the future
-        getLinks(path);
+        processMarkdownFile(path, printResults);
       }
     }
   } catch (e) {
